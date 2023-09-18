5027a66c5785a3f41f9eefe1713da97e48bb9ede
thestackdev <shanmukeshwar03@gmail.com>
2023-09-18 12:03:57 +0530

    initial commit

---
+++ components/media-room.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { LiveKitRoom, VideoConference } from "@livekit/components-react";
+import "@livekit/components-styles";
+import { Channel } from "@prisma/client";
+import { useUser } from "@clerk/nextjs";
+import { Loader2 } from "lucide-react";
+
+interface MediaRoomProps {
+  chatId: string;
+  video: boolean;
+  audio: boolean;
+};
+
+export const MediaRoom = ({
+  chatId,
+  video,
+  audio
+}: MediaRoomProps) => {
+  const { user } = useUser();
+  const [token, setToken] = useState("");
+
+  
+  
+  useEffect(() => {
+    console.log('chatId', user);
+    if (!user?.firstName) return;
+
+    const name = `${user.firstName}`;
+
+    (async () => {
+      try {
+        console.log('data');
+        const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`);
+        const data = await resp.json();
+        setToken(data.token);
+      } catch (e) {
+        console.log(e);
+      }
+    })()
+  }, [user?.firstName, chatId]);
+
+  if (token === "") {
+    return (
+      <div className="flex flex-col flex-1 justify-center items-center">
+        <Loader2
+          className="h-7 w-7 text-zinc-500 animate-spin my-4"
+        />
+        <p className="text-xs text-zinc-500 dark:text-zinc-400">
+          Loading...
+        </p>
+      </div>
+    )
+  }
+
+  return (
+    <LiveKitRoom
+      data-lk-theme="default"
+      serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}
+      token={token}
+      connect={true}
+      video={video}
+      audio={audio}
+    >
+      <VideoConference />
+    </LiveKitRoom>
+  )
+}
